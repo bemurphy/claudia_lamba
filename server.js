032c6e40d8d627085f81edfcbe1e52aae82d0c28
Brendon Murphy <xternal1+github@gmail.com>
2017-12-10 20:47:14 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var ApiBuilder = require('claudia-api-builder'),
+    superb = require('superb'),
+    api = new ApiBuilder();
+
+module.exports = api;
+
+api.get('/awesome', function(req) {
+  return req.queryString.name + ' is ' + superb();
+}, {apiKeyRequired: false});
+
+api.get('/sad', function(req) {
+  return req.queryString.name + ' wishes they were ' + superb();
+});
